9cf9d512c8f90d97ebe173cd06f8b780b9a92ee0
Kuete Cyrille <cyrillekuete@gmail.com>
2023-01-25 16:47:59 +0100

    Fetching completed

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./TripList.css";
 
 function TripList() {
-    fetch('http://localhost:3000/trips')
-        .then(response => response.json())
-    .then(json => conslole.log(json))
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/trips")
+      .then((response) => response.json())
+      .then((json) => setTrips(json));
+
+    //   return () => {
+    //     second
+    //   }
+  }, []);
+
   return (
     <div>
       <h2>Trips</h2>
+      <ul>
+        {trips.map((trip) => (
+          <li key={trip.id}>
+            <h3>{trip.title}</h3>
+            <p>{trip.price}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
