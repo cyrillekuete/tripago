21956dd8e2b04ff9961eae375482255519f9e89c
Kuete Cyrille <cyrillekuete@gmail.com>
2023-01-28 01:56:56 +0100

    Cleanup Function Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import TripList from "./components/TripList";
+import { useState } from "react";
 
 function App() {
+  const [showTrips, setShowTrips] = useState(true);
+  const [hideTrips, setHideTrips] = useState(false);
   return (
     <div className="App">
-      <TripList />
+      <button onClick={() => setShowTrips(false)}>Hide Trips</button>
+      {showTrips && <TripList />}
+
+      <button onClick={() => setHideTrips(true)}>Show Trips</button>
+      {hideTrips && <TripList />}
     </div>
   );
 }
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,19 +3,29 @@ import { useState, useEffect } from "react";
 const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
   useEffect(() => {
     const fetchData = async () => {
       setIsPending(true);
-      const res = await fetch(url);
-      const json = await res.json();
-      setIsPending(false);
-      setData(json);
+      try {
+        const res = await fetch(url);
+        if (!res.ok) {
+          throw new Error(res.statusText);
+        }
+        const json = await res.json();
+        setIsPending(false);
+        setData(json);
+        setError(null);
+      } catch (error) {
+        setError("Could not fetch data");
+      }
     };
     fetchData();
   }, [url]);
   return {
     data,
-    isPending,
+      isPending,
+    error
   };
 };
 
